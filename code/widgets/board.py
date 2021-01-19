bd27fb93a5cd3849a8fd6e922f130d71a28ef0e1
MGbenga <oyebanji_g@yahoo.com>
2021-01-19 13:45:50 +0100

    Fixed the mistake on the board seed placement
    Signed-off-by: MGbenga <oyebanji_g@yahoo.com>

--- code/widgets/board.py
+++ code/widgets/board.py
@@ -138,7 +138,7 @@ class Board(QFrame):
 
         clickLoc = f'{chr(65 + col - 1)}{row}'  # map to letter-number format
 
-        if (0 < row < self.board_width) and (0 < col < self.board_width):
+        if (0 < row < self.board_width + 1) and (0 < col < self.board_width + 1):
             tryValue = self.game_logic.tryMove(row - 1, col - 1)
             if tryValue == 0:
                 # if tryMove succeeds then update the next player colour
